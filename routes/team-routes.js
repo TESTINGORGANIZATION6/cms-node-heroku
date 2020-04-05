3b4023694b0a407fafdb5286d7d6e7ad45a03a2c
pprachit09 <pprachit09@gmail.com>
2020-04-05 02:05:26 -0300

    [CMS-4] Added getteams API

--- routes/team-routes.js
+++ routes/team-routes.js
@@ -11,10 +11,12 @@ const {
 const {
     addTeam,
     getTeam,
+    getTeams,
     teamById
 } = require('../controllers/team')
 
 router.post('/addteam/:userId', requiredSignIn, isAuth, addTeam)
+router.get('/getteams/:userId', requiredSignIn, isAuth, getTeams)
 router.get('/getteam/:teamId/:userId', requiredSignIn, isAuth, getTeam)
 
 router.param('userId', userById);
